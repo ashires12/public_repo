5b31b61779ba3ccbfa8a15b53417cef40798e1bf
Alexander Shires <alex.shires12@gmail.com>
2017-09-24 20:56:18 -0400

    bulk of all python files included.

---
+++ 422HW2/cars.py
@@ -0,0 +1,19 @@
+import DT
+import numpy as np
+
+fid = open("ResultsID3.txt", "a+")
+tree = DT.dtree(fid)
+inputs,targets,features = tree.read_data("cars.txt")
+t = tree.make_tree(inputs,targets,features)
+tree.printTree(t, " ")
+actual = tree.classifyAll(t,inputs)
+classified = tree.classifyAll(t,inputs)
+correct = 0
+for i in range(len(classified)):
+     if targets[i] == classified[i]:
+        correct += 1
+
+print >> fid, "\nNumber of training examples used: " + str(tree.numExUsed)
+print >> fid, "Number correctly classified: " + str(correct)
+print >> fid, "Number of nodes: " + str(tree.numNodes)
+print >> fid, "Number of Leaves: " +str(tree.numLeaves)
