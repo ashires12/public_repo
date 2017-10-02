ecd10637df86ca2443d88e41bde9879883d5627b
Alexander Shires <alex.shires12@gmail.com>
2017-10-01 20:03:51 -0400

    Finished up number two.

--- 422HW/HW3/logicEQ.py
+++ 422HW/HW3/logicEQ.py
@@ -1,19 +1,19 @@
 import numpy as np
 import preceptrons
 
-fid = open("ResultsAnd.txt","w")
+fid = open("ResultsEQ.txt","w")
 inputs = np.array([[0,0],[0,1],[1,0],[1,1]])
-targets = np.array([[0],[0],[0],[1]])
+targets = np.array([[1],[0],[0],[1]])
 nNet = preceptrons.pcn(inputs,targets,fid)
 print >> fid, "INFORMATION"
 print >> fid, "=================="
-print >> fid, "Learning rate = .2 \nNumber of training data = 4 \nNumber of iterations = 20 \n"
+print >> fid, "Learning rate = .25 \nNumber of training data = 4 \nNumber of iterations = 25 \n"
 print >> fid, "PRIOR TO TRAINING:"
 print >> fid, "=================="
 nNet.confmat(inputs,targets)
 print >> fid, "START AND END WEIGHTS:"
 print >> fid, "======================"
-nNet.pcntrain(inputs,targets,.2,20)
+nNet.pcntrain(inputs,targets,.25,25)
 print >> fid, "AFTER TRAINING:"
 print >> fid, "==============="
 nNet.confmat(inputs,targets)
--- 422HW/HW3/preceptrons.py
+++ 422HW/HW3/preceptrons.py
@@ -1,102 +1,136 @@
-
-# Code from Chapter 3 of Machine Learning: An Algorithmic Perspective (2nd Edition)
-# by Stephen Marsland (http://stephenmonika.net)
-
-# You are free to use, change, or redistribute the code in any way you wish for
-# non-commercial purposes, but please maintain the name of the original author.
-# This code comes with no warranty of any kind.
-
-# Stephen Marsland, 2008, 2014
-
 import numpy as np
 
 class pcn:
-	""" A basic Perceptron"""
 
-	def __init__(self,inputs,targets):
-		""" Constructor """
-		# Set up network size
-		if np.ndim(inputs)>1:
-			self.nIn = np.shape(inputs)[1]
-		else:
-			self.nIn = 1
+    """ A basic Perceptron"""
 
-		if np.ndim(targets)>1:
-			self.nOut = np.shape(targets)[1]
-		else:
-			self.nOut = 1
+    def __init__(
+        self,
+        inputs,
+        targets,
+        fid,
+        ):
+        """ Constructor """
 
-		self.nData = np.shape(inputs)[0]
+        # Set up network size
 
-		# Initialise network
-		self.weights = np.random.rand(self.nIn+1,self.nOut)*0.1-0.05
+        if np.ndim(inputs) > 1:
+            self.nIn = np.shape(inputs)[1]
+        else:
+            self.nIn = 1
 
-	def pcntrain(self,inputs,targets,eta,nIterations):
-		""" Train the thing """
-		# Add the inputs that match the bias node
-		inputs = np.concatenate((inputs,-np.ones((self.nData,1))),axis=1)
-		# Training
-		change = range(self.nData)
+        if np.ndim(targets) > 1:
+            self.nOut = np.shape(targets)[1]
+        else:
+            self.nOut = 1
 
-		for n in range(nIterations):
+        self.nData = np.shape(inputs)[0]
+        self.fid = fid
 
-			self.activations = self.pcnfwd(inputs);
-			self.weights -= eta*np.dot(np.transpose(inputs),self.activations-targets)
+        # Initialise network
 
-			# Randomise order of inputs
-			#np.random.shuffle(change)
-			#inputs = inputs[change,:]
-			#targets = targets[change,:]
+        self.weights = np.random.rand(self.nIn + 1, self.nOut) * 0.1 \
+            - 0.05
 
-		#return self.weights
+    def pcntrain(
+        self,
+        inputs,
+        targets,
+        eta,
+        nIterations,
+        ):
+        """ Train the thing """
 
-	def pcnfwd(self,inputs):
-		""" Run the network forward """
-		# Compute activations
-		activations =  np.dot(inputs,self.weights)
+        # Add the inputs that match the bias node
 
-		# Threshold the activations
-		return np.where(activations>0,1,0)
+        inputs = np.concatenate((inputs, -np.ones((self.nData, 1))),
+                                axis=1)
 
+        # Training
 
-	def confmat(self,inputs,targets):
-		"""Confusion matrix"""
+        change = range(self.nData)
 
-		# Add the inputs that match the bias node
-	    inputs = np.concatenate((inputs,-np.ones((inputs.shape[0],1))),axis=1)
+        fid = self.fid
+        counter = 1
 
+        for n in range(nIterations):
 
-		outputs = np.dot(inputs,self.weights)
+            self.activations = self.pcnfwd(inputs)
+            self.weights -= eta * np.dot(np.transpose(inputs),
+                    self.activations - targets)
 
-		nClasses = np.shape(targets)[1]
+            # Randomise order of inputs
+            # np.random.shuffle(change)
+            # inputs = inputs[change,:]
+            # targets = targets[change,:]
 
-		if nClasses==1:
-			nClasses = 2
-			outputs = np.where(outputs>0,1,0)
-		else:
-			# 1-of-N encoding
-			outputs = np.argmax(outputs,1)
-			targets = np.argmax(targets,1)
+            if (counter == 1):
+				print >> fid, "Initial weights:"
+				print >> fid, str(self.weights) + "\n"
+				counter += 1
+            elif (counter == nIterations):
+				print >> fid, "Final weights:"
+				print >> fid, str(self.weights) + "\n"
+				counter += 1
+            else:
+				counter += 1
 
-		cm = np.zeros((nClasses,nClasses))
-		for i in range(nClasses):
-			for j in range(nClasses):
-				cm[i,j] = np.sum(np.where(outputs==i,1,0)*np.where(targets==j,1,0))
+    def pcnfwd(self, inputs):
+        """ Run the network forward """
 
-		print cm
-		print np.trace(cm)/np.sum(cm)
+        # Compute activations
+
+        activations = np.dot(inputs, self.weights)
+
+        # Threshold the activations
+
+        return np.where(activations > 0, 1, 0)
+
+    def confmat(self, inputs, targets):
+        """Confusion matrix"""
+
+        # Add the inputs that match the bias node
+
+        inputs = np.concatenate((inputs, -np.ones((inputs.shape[0],
+                                1))), axis=1)
+        fid = self.fid
+
+        outputs = np.dot(inputs, self.weights)
+
+        nClasses = np.shape(targets)[1]
+
+        if nClasses == 1:
+            nClasses = 2
+            outputs = np.where(outputs > 0, 1, 0)
+        else:
+
+            # 1-of-N encoding
+
+            outputs = np.argmax(outputs, 1)
+            targets = np.argmax(targets, 1)
+
+        cm = np.zeros((nClasses, nClasses))
+        for i in range(nClasses):
+            for j in range(nClasses):
+                cm[i, j] = np.sum(np.where(outputs == i, 1, 0)
+                                  * np.where(targets == j, 1, 0))
+
+        print >> fid, "Confusion Matrix: "
+        print >> fid, str(cm) + "\n"
+        print >> fid, "Number classified correctly: "
+        print >> fid, str(np.trace(cm)) + " out of " + str(np.sum(cm)) + "\n"
+
 
 def logic():
-	import pcn
-	""" Run AND and XOR logic functions"""
+    import pcn
 
-	a = np.array([[0,0,0],[0,1,0],[1,0,0],[1,1,1]])
-	b = np.array([[0,0,0],[0,1,1],[1,0,1],[1,1,0]])
+    a = np.array([[0, 0, 0], [0, 1, 0], [1, 0, 0], [1, 1, 1]])
+    b = np.array([[0, 0, 0], [0, 1, 1], [1, 0, 1], [1, 1, 0]])
 
-	p = pcn.pcn(a[:,0:2],a[:,2:])
-	p.pcntrain(a[:,0:2],a[:,2:],0.25,10)
-	p.confmat(a[:,0:2],a[:,2:])
+    p = pcn.pcn(a[:, 0:2], a[:, 2:])
+    p.pcntrain(a[:, 0:2], a[:, 2:], 0.25, 10)
+    p.confmat(a[:, 0:2], a[:, 2:])
 
-	q = pcn.pcn(b[:,0:2],b[:,2:])
-	q.pcntrain(b[:,0:2],b[:,2:],0.25,10)
-	q.confmat(b[:,0:2],b[:,2:])
+    q = pcn.pcn(b[:, 0:2], b[:, 2:])
+    q.pcntrain(b[:, 0:2], b[:, 2:], 0.25, 10)
+    q.confmat(b[:, 0:2], b[:, 2:])
